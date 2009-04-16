f14f53f65b4d59c188f925e6add7ebb2e8d5ae31
dreamexcel <dreamexcel@7e7373ea-2901-11de-897e-ef2ff8275b85>
2009-04-16 00:28:17 +0000

    Hmong Dictionary

---
+++ trunk/HmongDict/HmongDict/Database.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Data.SQLite;
+
+namespace HmongDict
+{
+    class Database
+    {
+        SQLiteConnection SQLiteCon = null;
+        
+        public Database()
+        {
+            SQLiteCon = new SQLiteConnection();
+
+            bool bDbExisted = File.Exists(@"Dict.db");
+            if (!bDbExisted)
+                SQLiteConnection.CreateFile(@"Dict.db");
+
+            SQLiteCon.ConnectionString = @"Data Source=Dict.db; Password=Hmong";
+            SQLiteCon.Open();
+
+            if (bDbExisted)
+            {
+                InsertTableListRows();
+            }
+        }
+
+        ~Database()
+        {
+            if (null != SQLiteCon)
+            {
+                SQLiteCon.Close();
+                SQLiteCon.Dispose();
+                SQLiteCon = null;
+            }
+        }
+
+        public SQLiteDataReader Query(string strCmd)
+        {
+            if (null == SQLiteCon)
+            {
+                throw new Exception("SQLiteConnection not init yet!");
+            }
+
+            SQLiteCommand cmd = new SQLiteCommand(strCmd, SQLiteCon);
+            return cmd.ExecuteReader();
+        }
+
+        private bool InsertTableListRows()
+        {
+            for (int a = 0; a < 50; a++)
+            {
+                string strCreateTableCmd = @"INSERT INTO `TableList` (`ID`, `CnName`, `EnName`, `HmName`, `TableName`) values(NULL, '反对', 'fdl', 'fds', 'fdsa" + a.ToString() + "')";
+                SQLiteCommand cmd = new SQLiteCommand(strCreateTableCmd, SQLiteCon);
+                cmd.ExecuteNonQuery();
+            }
+
+            return true;
+        }
+        
+    }
+}
---
+++ trunk/HmongDict/HmongDict/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace HmongDict
+{
+    public partial class Form1 : Form
+    {
+        Database m_Database = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBoxWord_SizeChanged(object sender, EventArgs e)
+        {
+            this.comboBoxWord.DropDownWidth = this.comboBoxWord.Width;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m_Database = new Database();
+
+            if (File.Exists("English_Hmong.wb"))
+            {
+                FileInfo fi = new FileInfo("English_Hmong.wb");
+                StreamReader sr = new StreamReader("English_Hmong.wb", Encoding.Default);
+                Char[] btData = new Char[84];
+
+                int ii = 0;
+
+                while (sr.Read(btData, 0, 84) > 0)
+                {
+                    if (ii++ > 20)
+                        break;
+
+                    for (int i = 0; i < 84;i++ )
+                    {
+                        if (btData[i] == 0)
+                        {
+                            btData[i] = ' ';
+                        }
+                    }
+
+                    string str = new string(btData);
+                    str = str.Replace("  ", " ");
+
+                    this.richTextBox1.AppendText( str + "\r\n");
+                }
+
+                sr.Close();
+
+                this.webBrowserShowResult.DocumentText += "English_Hmong.wb 文件存在\n" + fi.FullName + "\nSize = " + fi.Length.ToString();
+            }
+        }
+
+        private void comboBoxWord_TextChanged(object sender, EventArgs e)
+        {
+            SQLiteDataReader reader = m_Database.Query("Select * From TableList");
+            if (reader.HasRows)
+            {
+                do
+                {   
+                    reader.Read();
+                    this.webBrowserShowResult.DocumentText += reader["ID"].ToString() + reader["CnName"].ToString() + reader["EnName"].ToString() + reader["HmName"].ToString() + reader["TableName"].ToString() + "<br />\r\n"; ;
+                }
+                while (reader.NextResult()) ;
+            }
+        }
+    }
+}
